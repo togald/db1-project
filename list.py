c81b1a1c215f3ce06ec5b67d414d96ae0d63ef29
Togald Nilsson <togald.nilsson@gmail.com>
2021-11-30 14:56:51 +0100

    Added helpers

--- list.py
+++ list.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
-import dbthing
+import dbthing as dbt
 
-data = dbthing.tunneled_sql('show tables')
-
-dbthing.sql_print(data)
+if __name__ == "__main__":
+    dids = [ did[0] for did in dbt.tunneled_sql('select DepartmentID from department') ]
+    did = ''
+    while True:
+        did = int(input('Department ID: ')) or ''
+        if did in dids: break
+        print('Invalid department ID, try again!')
+    
+    if () == dbt.tunneled_sql(f"select DepartmentID from department where parent_depart={did}"):
+        data = dbt.tunneled_sql(f"select ProductID,ProductTitle,Unit_price,Discount,tax from product where id_department={did}")
+        chdata = [ ('Product ID', 'Product title', 'Retail price') ]
+        for row in data: 
+            chdata.append(( row[0], row[1], (1+float(row[4]))*(float(row[2])*float(row[3])) ))
+    else:
+        data = dbt.tunneled_sql(f"Select DepartmentId,Department_Title from department where parent_depart={did}")
+        chdata = [ ('Department ID', 'Department name') ]
+        for row in data:
+            chdata.append(( row[0], row[1] ))
+    dbt.sql_print(chdata)
